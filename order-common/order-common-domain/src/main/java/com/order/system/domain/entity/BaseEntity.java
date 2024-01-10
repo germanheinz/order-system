5e0af2982d13e6db30927ce1205a0c2e1af795be
USERSAD\gerheinz <heinz.german@gmail.com>
2024-01-10 23:14:39 +0100

    common-domain

---
+++ order-common/order-common-domain/src/main/java/com/order/system/domain/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.order.system.domain.entity;
+
+public abstract class BaseEntity<ID> {
+
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
