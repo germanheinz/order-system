2170af90a1f22eecbb871b07744a18cfb671b248
USERSAD\gerheinz <heinz.german@gmail.com>
2024-01-15 23:16:27 +0100

    Entity models

---
+++ order-domain/order-domain-core/src/main/java/valueobject/StreetAddress.java
@@ -0,0 +1,46 @@
+package valueobject;
+
+
+import java.util.UUID;
+
+public class StreetAddress {
+
+    private final UUID id;
+    private final String street;
+    private final String postalCode;
+    private final String city;
+
+
+    public StreetAddress(UUID id, UUID id1, String street, String postalCode, String city) {
+        this.id=id;
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ order-domain/order-domain-core/src/main/java/valueobject/TrackingId.java
@@ -0,0 +1,11 @@
+package valueobject;
+
+import com.order.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+    public TrackingId(UUID id){
+        super(id);
+    }
+}
