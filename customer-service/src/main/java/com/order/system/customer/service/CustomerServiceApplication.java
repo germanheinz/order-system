a24ad45893bbeec61b2f2ee0a78f09d435aa3a53
germanheinz <heinz.german@gmail.com>
2024-02-19 22:57:30 +0100

    spring boot parent

--- customer-service/src/main/java/com/order/system/customer/service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/order/system/customer/service/CustomerServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.order.system")
 public class CustomerServiceApplication {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
+
 }
